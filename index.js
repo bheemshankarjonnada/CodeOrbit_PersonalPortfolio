db40ed95eef59017c82329ade755e7d822c66a6f
Bheem <bheemshankarj2326@gmail.com>
2026-08-14 20:34:08 +0530

    Initial commit - Portfolio website

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+document.getElementById("contacting").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let name=document.getElementById("name").value.trim();
+    let mail=document.getElementById("mail").value.trim();
+    let phone=document.getElementById("phone").value.trim();
+    let status=document.getElementById("status");
+
+    let phonepattern=/^[0-9]{10}$/;
+
+    if(name==="" || mail==="" || phone==="" ){
+        status.textContent=" ⚠️ please fill all the fields and submit";
+        status.style.color="red";
+    }
+        
+    else if(!phonepattern.test(phone)){
+        status.textContent="Please enter vaild mobile number !";
+        status.style.color="red";
+    }
+
+    else {
+         status.textContent="Submitted Successfully 👍";
+        status.style.color="yellow";
+    }
+});
